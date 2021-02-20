dd5cbc18fecd070ceccded1ff06247ed04f943dc
OrlovIgorN <orlikman@yandex.ru>
2021-02-20 18:03:30 +0300

    3

--- DZ16.5/DZ16.5/DZ16.5.cpp
+++ DZ16.5/DZ16.5/DZ16.5.cpp
@@ -5,19 +5,20 @@
 using namespace std;
 int main()
 {
-    int a[10][10];
-    for ( int i=0;i<10;i++)
+    const int N = 10;
+    int a[N][N];
+    for ( int i=0;i<N;i++)
     {
-        for (int j = 0; j < 10; j++)
+        for (int j = 0; j < N; j++)
         {       
-                a[i][j] = (i+1)+(j+1);
+                a[i][j] = i+j;
                 std::cout << a[i][j]<<" "; 
         }
         std::cout << "\n";
     }
     const int stroka = 20 % 10;
     int sum = 0;
-    for (int i = 0; i < 10; ++i)
+    for (int i = 0; i < N; ++i)
     {
         sum += a[stroka][i];
     }
